b2fdb34140aee12de341e74c67d41178a215e6a6
Steven Astur <steven.astur@gmail.com>
2024-02-25 16:55:40 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 class ProductManager {
   constructor() {
     this.products = [];
+    let path = (this.path = "./desafio");
   }
 
   static id = 0;
 
-  addProduct(title, description, price, image, code, stock) {
+  addProduct(newProduct) {
+    const { title, description, price, thumbnail, code, stock } = newProduct
+    
     for (let i = 0; i < this.products.length; i++) {
       if (this.products[i].code === code) {
-        console.error(`el codigo "${code}" ya fue registrado, por favor utilizar otro.`);
-        break;
+        console.error(
+          `el codigo "${code}" ya fue registrado, por favor utilizar otro.`
+          );
+          return;
+        }
       }
-    }
-
-    const newProduct = {
-      title,
-      description,
-      price,
-      image,
-      code,
-      stock,
-    };
 
     if (!Object.values(newProduct).includes(undefined)) {
-        ProductManager.id++;
-        this.products.push({
-            ...newProduct,
-            id: ProductManager.id,
-        });
+      ProductManager.id++;
+      this.products.push({
+        ...newProduct,
+        id: ProductManager.id,
+      });
     } else {
-        console.error("Algun campo no esta completo, completelo por favor!");
+      console.error("Algun campo no esta completo, completelo por favor!");
     }
-
   }
 
   getProducts() {
@@ -43,7 +38,9 @@ class ProductManager {
   }
 
   getProductById(id) {
-    !this.exists(id) ? console.error(`El ID "${id}" no existe.`) : console.log(`El ID "${id}" fue encontrado con exito!`);
+    !this.exists(id)
+      ? console.error(`El ID "${id}" no existe.`)
+      : console.log(`El ID "${id}" fue encontrado con exito!`);
   }
 }
 const productos = new ProductManager();
@@ -52,19 +49,60 @@ const productos = new ProductManager();
 console.log(productos.getProducts());
 
 //Se agregarn productos
-productos.addProduct("title1", "description1", 4000, "image1", "abc123", 5);
-productos.addProduct("title2", "description2", 7000, "image2", "abc124", 11);
-productos.addProduct("title3", "description3", 2000, "image3", "abc125", 13);
+// productos.addProduct("title1", "description1", 4000, "image1", "abc123", 5);
+// productos.addProduct("batata", "description2", 7000, "image2", "abc124", 11);
+// productos.addProduct("cebolla", "description3", 2000, "image3", "abc125", 13);
+
+const productData1 = {
+  title: "Papa blanca",
+  description: "Papa blanca de alta calidad",
+  price: 600,
+  thumbnail: "papaBlanca.jpg",
+  code: "C002",
+  stock: 70,
+};
+
+productos.addProduct(productData1);
+
+const productData2 = {
+  title: "Papa negra",
+  description: "Papa negra de alta calidad y economica",
+  price: 400,
+  thumbnail: "papaNegra.jpg",
+  code: "C003",
+  stock: 30,
+};
+
+productos.addProduct(productData2);
+
+const productData3 = {
+  title: "batata",
+  description: "Deliciosa batata fresca",
+  price: 700,
+  thumbnail: "batata.jpg",
+  code: "C004",
+  stock: 60,
+};
+
+productos.addProduct(productData3);
 
 //Segunda llamada con 2 productos agregados
 console.log(productos.getProducts());
 
 //Valida CODE repetido + 2 productos
-productos.addProduct("title4", "description4", 1000, "image4", "abc123", 2);
+const productData4 = {
+  title: "zanahoria",
+  description: "Deliciosa zanahoria fresca",
+  price: 300,
+  thumbnail: "zanahoria.jpg",
+  code: "C005",
+  stock: 100,
+};
+
+productos.addProduct(productData4);
 
 //Valida con busqueda por ID
 productos.getProductById(2);
-
 
 //Valida con busqueda por ID con error
 productos.getProductById(9);
